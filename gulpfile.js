7f43abbd8407cf962ff242217e819ec7c3ac7aaa
Kateryna Kukunina <kateryna.k@sbtech.com>
2019-09-03 22:16:47 +0300

    Project initialization

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var swig = require('gulp-swig');
+
+gulp.task('sass', function () {
+    gulp.src('./src/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('swig', function() {
+    gulp.src('./src/index.html')
+        .pipe(swig({defaults: { cache: false }}))
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./src/**/*.scss', ['sass', 'swig']);
+});
+
+gulp.task('default', ['sass', 'swig']);
